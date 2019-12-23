8e94da5bd787297a76b821cbab570678771a16eb
VadimGuryanov <42974944+VadimGuryanov@users.noreply.github.com>
2019-12-23 12:17:22 +0300

    Revert "ui"

--- src/main/java/ru/kpfu/itis/gallows/ui/interfaces/IViewClient.java
+++
@@ -1,6 +0,0 @@
-package ru.kpfu.itis.gallows.ui.interfaces;
-
-public interface IViewClient {
-    void sendWord(char word);
-    void sendCodeRoom(String code);
-}
--- src/main/java/ru/kpfu/itis/gallows/ui/interfaces/PanelManager.java
+++
@@ -1,9 +0,0 @@
-package ru.kpfu.itis.gallows.ui.interfaces;
-
-import javax.swing.*;
-
-public interface PanelManager {
-
-    void switchTo(JPanel panel);
-
-}
